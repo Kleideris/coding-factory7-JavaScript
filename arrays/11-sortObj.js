bc463ab3efe39ba36f8501ca1c298d88ea842fa9
Ch. Kleideris <kleiderisch@gmail.com>
2025-01-30 21:00:27 +0200

    JS course 30/1

---
+++ arrays/11-sortObj.js
@@ -0,0 +1,24 @@
+const objArray = [
+  {id: 1, first: "Νίκος", age: 23},
+  {id: 2, first: "Αλίκη", age: 40},
+  {id: 3, first: "Κώστας", age: 20},
+  {id: 4, first: "Κώστας", age: 18}
+]
+
+// sort by age
+objArray.sort(function(a, b) {
+  return a.age - b.age
+})
+
+// console.log(objArray)
+
+// Sort by first and age
+
+objArray.sort(function(a, b) {
+  if (a.first === b.first) {
+    return a.age - b.age
+  }
+  return a.first.localeCompare(b.first, 'el')
+})
+
+console.log(objArray)
