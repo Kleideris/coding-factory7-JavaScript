b09d78fc40268219006c77f99b2bb80c1e20ef8f
Ch. Kleideris <kleiderisch@gmail.com>
2025-01-30 21:19:48 +0200

    minor bugfixes

--- arrays/12b-deleteMultipleItems.js
+++ arrays/12b-deleteMultipleItems.js
@@ -3,7 +3,7 @@ const arr = [1, 2, 3, 2]
 const deleteBackwards = (arr, val) => {
   if (!arr) return
   let i = arr.length - 1
-  while (i) {
+  while (i >= 0) {
     if (arr[i] ===val) {
       arr.splice(i, 1)
     }
@@ -11,5 +11,5 @@ const deleteBackwards = (arr, val) => {
   }
 }
 
-deleteBackwards(arr, 2)
+deleteBackwards(arr, 1)
 console.log(arr)
--- arrays/9-getIndexes.js
+++ arrays/9-getIndexes.js
@@ -3,35 +3,20 @@ const arr = [1, 2, 4, 5, 2, 6, 7, 2, 5, 8, 9, 9, 2]
 // function να επιστρέφει ένα πίνακα με τις θέσεις
 // στις οποιές βρίσκεται ένασ συγκεκριμένος αριθμός
 
-// function returnIndexesOfEl(el) {
-//   position = 0;
-//   returnArr = []
-
-//   while (arr.includes(el)) {
-//     arr.splice(position, el)
-//     position = arr.indexOf(el)
-//     returnArr.push(position)
-//   }
-//   return returnArr
-// }
-
-// console.log(arr.returnIndexesOfEl(2))
-
 const getIndexes = (arr, val) => {
   const indexes = []
 
-  // arr.forEach((v, index) => {
-  //   if (v === val) {
-  //     indexes.push(index)
-  //   }
-  // })
-
-  arr.array.forEach(function(v, index) {
+  arr.forEach((v, index) => {
     if (v === val) {
       indexes.push(index)
     }
   })
 
+  // arr.forEach(function(v, index) {
+  //   if (v === val) {
+  //     indexes.push(index)
+  //   }
+  // })
   return indexes
 }
 
