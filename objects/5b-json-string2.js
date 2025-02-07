21e555a5b5582d224eca216967bc2bbcd018bcb2
Ch. Kleideris <kleiderisch@gmail.com>
2025-02-07 23:44:44 +0200

    hotfix: 5b-json-string

--- objects/5b-json-string2.js
+++ objects/5b-json-string2.js
@@ -1,20 +1,20 @@
 const store = `[
-{"id": "pro4vog,
+{"id": "pro4vog",
   "fields": {
     "company": "ikea",
-    "colors: ["#FF1234,"#222"],
+    "colors": ["#FF1234","#222"],
     "featured": true,
     "price": 990,
-    "genre": white-black-chair"
+    "genre": "white-black-chair"
   }
 },
-{"id": "pro4vog,
+{"id": "pro4vog",
   "fields": {
     "company": "ikea",
-    "colors: ["#AB199,"#222"],
+    "colors": ["#AB199","#222"],
     "featured": true,
     "price": 880,
-    "genre": black-black-chair"
+    "genre": "black-black-chair"
   }
 }
 
